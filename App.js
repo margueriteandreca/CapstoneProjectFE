1a528e7e7fb1f67f79a899f09bf9761c211ed69a
+ <+>
2022-08-31 14:49:15 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,175 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, View, Text, TouchableOpacity, Image } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const screenOptions = ({ route }) => ({
+  // THESE ARE THE CONFIGS OF HOW BOTTOM TABS SHOW
+  tabBarIcon: ({ focused, color, size }) => {
+    switch (route.name) {
+      case "HomeStack":
+        return <Entypo name="home" size={24} color="black" />;
+      case "SettingsStack":
+        return <FontAwesome name="user-circle-o" size={24} color="black" />;
+      default:
+        return <Image />;
+    }
+  },
+  tabBarActiveTintColor: "#3872E9",
+  tabBarInactiveTintColor: "gray",
+  tabBarShowLabel: false,
+});
+
+function SettingsScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings Screen!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home Screen!</Text>
+    </View>
+  );
+}
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+// BOTTOM TABS
+function BottomTabs() {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name="SettingsStack" component={SettingsStack} />
+    </Tab.Navigator>
+  );
+}
+
+// WHAT A LOGGEF IN USER SEES
+function LoggedInNavigator() {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={BottomTabs} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+// MOVE THESE SCREENS TO NEW FILES
+function LogInScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Log In Here!</Text>
+    </View>
+  );
+}
+
+function SignUpScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Sign Up here!</Text>
+    </View>
+  );
+}
+
+// STACK FOR A SIGN IN AND LOG IN SCREEN
+function LoggedOutNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="LogIn"
+        component={LogInScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+//WHERE A USER LANDS ON OPENING THE APP
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoggedIn"
+          component={LoggedInNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LoggedOut"
+          component={LoggedOutNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
